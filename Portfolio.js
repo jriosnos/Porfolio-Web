a39e9dd785c2c09124bc8d375004655cf241263f
Julien Rios <julienrios1@gmail.com>
2019-08-08 18:09:10 -0400

    Scrolling

--- Portfolio.js
+++ Portfolio.js
@@ -12,7 +12,7 @@ $("a").on('click', function(event) {
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 500, function(){
+      }, 5000, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
